d3ca676a736b511c506f0c58fd6eafd76353378c
Filip Cwiek <fifek.inex@gmail.com>
2020-05-15 18:40:18 +0200

    Start work on task 3

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './List.scss';
 import PropTypes from 'prop-types';
 import Hero from '../Hero/Hero.js'
+import Creator from '../Creator/Creator.js'
 import Column from '../Column/Column.js'
 import {settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
@@ -21,6 +22,21 @@ class List extends React.Component {
       description: settings.defaultListDescription,
 
     }
+    addColumn(title){
+      this.setState(state => (
+        {
+          columns: [
+            ...state.columns,
+            {
+              key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
+              title,
+              icon: 'list-alt',
+              cards: []
+            }
+          ]
+        }
+      ));
+    }
     
   render() {
     return (
@@ -30,9 +46,12 @@ class List extends React.Component {
             {ReactHtmlParser(this.props.description)}
         </div>
         <div className={styles.columns}>
-            <Column title={"Animals"}></Column>
-            <Column title={"Plants"}></Column>
-            <Column title={"Minerals"}></Column>
+          {this.state.columns.map(({key, ...columnProps}) => (
+          <Column key={key} {...columnProps} />
+          ))}
+        </div>
+        <div className={styles.creator}>
+          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
         </div>
       </section>
     )
