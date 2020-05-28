60bff496be87861a7570cb7c981cac73b2cf8e2c
Filip Cwiek <fifek.inex@gmail.com>
2020-05-28 15:25:47 +0200

    Refactoring ListContainer.js

--- src/components/List/ListContainer.js
+++ src/components/List/ListContainer.js
@@ -1,15 +1,21 @@
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import List from './List';
-import { getColumnsForList } from '../../../Redux/columnRedux';
-import { createActionAddColumn } from '../../../Redux/columnRedux';
+import {getColumnsForList} from '../../../Redux/columnRedux';
+import {createActionAddColumn} from '../../../Redux/columnRedux';
 
-const mapStateToProps = (state, props) => ({
-  columns: getColumnsForList(state, props.id),
-});
+const mapStateToProps = (state, props) => {
+  const id = props.match.params.id;
+  const filteredLists = state.lists.filter(list => list.id == id);  
+  const listParams = filteredLists[0] || {};
+  return {
+    ...listParams,
+    columns: getColumnsForList(state, id),
+  };
+};
 
 const mapDispatchToProps = (dispatch, props) => ({
   addColumn: title => dispatch(createActionAddColumn({
-    listId: props.id,
+    listId: props.match.params.id,
     title: title,
   })),
 });
